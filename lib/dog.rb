62a3492705109fc3fcccccfd355f58d87d36c9c5
Sidneyyy <catitude14@outlook.com>
2018-07-13 19:16:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # dog.rb
+
+class Dog
+  
+  def bark=(bark)
+    @bark = bark
+  end
+  
+  def bark
+    @bark
+   end
+   
+   def sit=(sit)
+     @sit = sit
+   end
+   
+   def sit
+     @sit
+   end
+ end
+ 
+ woof = Dog.new
+ sit = Dog.new
+ 
+ puts woof.bark = "Woof!"
+ puts sit.sit = "The Dog is sitting"
